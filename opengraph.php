42e2808cbb83fef53027f6b1bde2bc0bd3c77ec8
Will Norris <will@willnorris.com>
2011-11-04 18:20:22 -0700

    whitespace cleanup. no substantive changes

--- opengraph.php
+++ opengraph.php
@@ -19,11 +19,11 @@ $opengraph_ns_set = false;
  * Add Open Graph XML namespace to <html> element.
  */
 function opengraph_add_namespace( $output ) {
-	global $opengraph_ns_set;
-	$opengraph_ns_set = true;
+  global $opengraph_ns_set;
+  $opengraph_ns_set = true;
 
-	$output .= ' xmlns:og="' . esc_attr(OPENGRAPH_NS_URI) . '"';
-	return $output;
+  $output .= ' xmlns:og="' . esc_attr(OPENGRAPH_NS_URI) . '"';
+  return $output;
 }
 add_filter('language_attributes', 'opengraph_add_namespace');
 
@@ -35,30 +35,30 @@ add_filter('language_attributes', 'opengraph_add_namespace');
  * @uses apply_filters() Calls 'opengraph_metadata' before returning metadata array
  */
 function opengraph_metadata() {
-	$metadata = array();
+  $metadata = array();
 
- 	// defualt properties defined at http://opengraphprotocol.org/
-	$properties = array(
-		// required
-		'title', 'type', 'image', 'url', 
+   // defualt properties defined at http://opengraphprotocol.org/
+  $properties = array(
+    // required
+    'title', 'type', 'image', 'url',
 
-		// optional
-		'site_name', 'description', 
+    // optional
+    'site_name', 'description',
 
-		// location
-		'longitude', 'latitude', 'street-address', 'locality', 'region', 
-		'postal-code', 'country-name',
+    // location
+    'longitude', 'latitude', 'street-address', 'locality', 'region',
+    'postal-code', 'country-name',
 
-		// contact
-		'email', 'phone_number', 'fax_number',
-	);
+    // contact
+    'email', 'phone_number', 'fax_number',
+  );
 
-	foreach ($properties as $property) {
-		$filter = 'opengraph_' . $property;
-		$metadata["og:$property"] = apply_filters($filter, '');
-	}
+  foreach ($properties as $property) {
+    $filter = 'opengraph_' . $property;
+    $metadata["og:$property"] = apply_filters($filter, '');
+  }
 
-	return apply_filters('opengraph_metadata', $metadata);
+  return apply_filters('opengraph_metadata', $metadata);
 }
 
 
@@ -66,13 +66,13 @@ function opengraph_metadata() {
  * Register filters for default Open Graph metadata.
  */
 function opengraph_default_metadata() {
-	add_filter('opengraph_title', 'opengraph_default_title', 5);
-	add_filter('opengraph_type', 'opengraph_default_type', 5);
-	add_filter('opengraph_image', 'opengraph_default_image', 5);
-	add_filter('opengraph_url', 'opengraph_default_url', 5);
+  add_filter('opengraph_title', 'opengraph_default_title', 5);
+  add_filter('opengraph_type', 'opengraph_default_type', 5);
+  add_filter('opengraph_image', 'opengraph_default_image', 5);
+  add_filter('opengraph_url', 'opengraph_default_url', 5);
 
-	add_filter('opengraph_site_name', 'opengraph_default_sitename', 5);
-	add_filter('opengraph_description', 'opengraph_default_description', 5);
+  add_filter('opengraph_site_name', 'opengraph_default_sitename', 5);
+  add_filter('opengraph_description', 'opengraph_default_description', 5);
 }
 add_filter('wp', 'opengraph_default_metadata');
 
@@ -81,12 +81,12 @@ add_filter('wp', 'opengraph_default_metadata');
  * Default title property, using the page title.
  */
 function opengraph_default_title( $title ) {
-	if ( is_singular() && empty($title) ) {
-		global $post;
-		$title = $post->post_title;
-	}
+  if ( is_singular() && empty($title) ) {
+    global $post;
+    $title = $post->post_title;
+  }
 
-	return $title;
+  return $title;
 }
 
 
@@ -94,8 +94,8 @@ function opengraph_default_title( $title ) {
  * Default type property.
  */
 function opengraph_default_type( $type ) {
-	if ( empty($type) ) $type = 'blog';
-	return $type;
+  if ( empty($type) ) $type = 'blog';
+  return $type;
 }
 
 
@@ -103,15 +103,15 @@ function opengraph_default_type( $type ) {
  * Default image property, using the post-thumbnail.
  */
 function opengraph_default_image( $image ) {
-	global $post;
-	if ( is_singular() && empty($image) && has_post_thumbnail($post->ID) ) {
-		$thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'post-thumbnail');
-		if ($thumbnail) {
-			$image = $thumbnail[0];
-		}
-	}
+  global $post;
+  if ( is_singular() && empty($image) && has_post_thumbnail($post->ID) ) {
+    $thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'post-thumbnail');
+    if ($thumbnail) {
+      $image = $thumbnail[0];
+    }
+  }
 
-	return $image;
+  return $image;
 }
 
 
@@ -119,8 +119,8 @@ function opengraph_default_image( $image ) {
  * Default url property, using the permalink for the page.
  */
 function opengraph_default_url( $url ) {
-	if ( empty($url) ) $url = get_permalink();
-	return $url;
+  if ( empty($url) ) $url = get_permalink();
+  return $url;
 }
 
 
@@ -128,8 +128,8 @@ function opengraph_default_url( $url ) {
  * Default site_name property, using the bloginfo name.
  */
 function opengraph_default_sitename( $name ) {
-	if ( empty($name) ) $name = get_bloginfo('name');
-	return $name;
+  if ( empty($name) ) $name = get_bloginfo('name');
+  return $name;
 }
 
 
@@ -137,8 +137,8 @@ function opengraph_default_sitename( $name ) {
  * Default description property, using the bloginfo description.
  */
 function opengraph_default_description( $description ) {
-	if ( empty($description) ) $description = get_bloginfo('description');
-	return $description;
+  if ( empty($description) ) $description = get_bloginfo('description');
+  return $description;
 }
 
 
@@ -146,18 +146,18 @@ function opengraph_default_description( $description ) {
  * Output Open Graph <meta> tags in the page header.
  */
 function opengraph_meta_tags() {
-	global $opengraph_ns_set;
+  global $opengraph_ns_set;
 
-	$xml_ns = '';
-	if ( !$opengraph_ns_set ) {
-		$xml_ns = 'xmlns:og="' . esc_attr(OPENGRAPH_NS_URI) . '" ';
-	}
+  $xml_ns = '';
+  if ( !$opengraph_ns_set ) {
+    $xml_ns = 'xmlns:og="' . esc_attr(OPENGRAPH_NS_URI) . '" ';
+  }
 
-	$metadata = opengraph_metadata();
-	foreach ( $metadata as $key => $value ) {
-		if ( empty($key) || empty($value) ) continue;
-		echo '<meta ' . $xml_ns . 'property="' . esc_attr($key) . '" content="' . esc_attr($value) . '" />' . "\n";
-	}
+  $metadata = opengraph_metadata();
+  foreach ( $metadata as $key => $value ) {
+    if ( empty($key) || empty($value) ) continue;
+    echo '<meta ' . $xml_ns . 'property="' . esc_attr($key) . '" content="' . esc_attr($value) . '" />' . "\n";
+  }
 }
 add_action('wp_head', 'opengraph_meta_tags');
 
--- readme.txt
+++ readme.txt
@@ -32,24 +32,24 @@ where {name} is the unqualified Open Graph property name.  For example, if you h
 defines a custom post type named "movie", you could override the Open Graph 'type' property for
 those posts using a function like:
 
-	function my_og_type( $type ) {
-	    if ( get_post_type() == "movie" ) {
-	        $type = "movie";
-	    }
-	    return $type;
-	}
-	add_filter( 'opengraph_type', 'my_og_type' );
+  function my_og_type( $type ) {
+      if ( get_post_type() == "movie" ) {
+          $type = "movie";
+      }
+      return $type;
+  }
+  add_filter( 'opengraph_type', 'my_og_type' );
 
-This will work for all of the core Open Graph properties.  However, if you want to add a custom 
+This will work for all of the core Open Graph properties.  However, if you want to add a custom
 property, such as 'fb:admin', then you would need to hook into the `opengraph_metadata` filter.
 This filter is passed an associative array, whose keys are the qualified Open Graph property names.
 For example:
 
-	function my_og_metadata( $metadata ) {
-	    $metadata['fb:admin'] = '12345,67890';
-	    return $metadata;
-	}
-	add_filter( 'opengraph_metadata', 'my_og_metadata' );
+  function my_og_metadata( $metadata ) {
+      $metadata['fb:admin'] = '12345,67890';
+      return $metadata;
+  }
+  add_filter( 'opengraph_metadata', 'my_og_metadata' );
 
 Note that it is your responsibility to make sure that the XML namespace is registered for the
 prefix that you use.  To see one method for doing this, see the `opengraph_add_namespace` function
